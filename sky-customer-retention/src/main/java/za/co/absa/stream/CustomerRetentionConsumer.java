49d3b8a9b82766ec736b5d50292789608c7385b4
veja001 <veli.khumalo@Velis-MacBook-Pro.local>
2017-01-12 16:32:57 +0200

    complete

--- sky-customer-retention/src/main/java/za/co/absa/stream/CustomerRetentionConsumer.java
+++ sky-customer-retention/src/main/java/za/co/absa/stream/CustomerRetentionConsumer.java
@@ -1,13 +1,19 @@
 package za.co.absa.stream;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.SubscribableChannel;
 
 @EnableBinding(CustomerRetentionConsumer.Channels.class)
 public class CustomerRetentionConsumer {
+
+
 
     @StreamListener(Channels.CANCELLATION_REQUEST)
     public void cancellation(String message) {
@@ -15,10 +21,14 @@ public class CustomerRetentionConsumer {
     }
 
     interface Channels {
+        //String CANCELLATION_REPLY = "cancellationReply";
         String CANCELLATION_REQUEST = "cancellationRequest";
 
         @Input(CANCELLATION_REQUEST)
         SubscribableChannel cancellationRequestChannel();
+
+        //@Output(CANCELLATION_REPLY)
+        //MessageChannel cancellationReplyChannel();
     }
 }
 
