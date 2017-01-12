8cab7c862671178b481fa08dfed15c2e47cfa01e
Philip Dimitri Lourandos <philip.lourandos@absa.co.za>
2017-01-12 17:19:52 +0200

    Added test case

--- sky-reporting-service/src/test/java/za/co/absa/SkyReportingServiceApplicationTests.java
+++ sky-reporting-service/src/test/java/za/co/absa/SkyReportingServiceApplicationTests.java
@@ -2,15 +2,27 @@ package za.co.absa;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SkyReportingServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private Source skySrc;
+    
+    @Autowired
+    private MessageCollector postman;
+    
+    @Test
+    public void successProduce() {
+        assertEquals(1, postman.forChannel(skySrc.output()).size());
+    }
 
 }
