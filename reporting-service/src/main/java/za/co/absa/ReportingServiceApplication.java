dffd7cc500ccfd9bc4ef4e9c2b9bbd264b0d5572
Veli Khumalo <veli.khumalo@Velis-MacBook-Pro.local>
2017-01-12 15:10:23 +0200

    Channels created

--- reporting-service/src/main/java/za/co/absa/ReportingServiceApplication.java
+++ reporting-service/src/main/java/za/co/absa/ReportingServiceApplication.java
@@ -2,6 +2,16 @@ package za.co.absa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
+
+import java.io.File;
 
 @SpringBootApplication
 public class ReportingServiceApplication {
@@ -10,3 +20,31 @@ public class ReportingServiceApplication {
 		SpringApplication.run(ReportingServiceApplication.class, args);
 	}
 }
+
+
+@EnableBinding(ReportingServiceProducer.Channels.class)
+class ReportingServiceProducer {
+
+	@InboundChannelAdapter(value = Channels.REQ_REPORT, poller = @Poller(fixedDelay = "10000"))
+	public String requestReportMessageSource(){
+		return "request:report";
+	}
+
+	@StreamListener(Channels.RETRIEVE_REPORT)
+	public void report(String message) {
+		System.out.println(">>>"+message);
+		new File("");
+	}
+
+	interface Channels {
+		String REQ_REPORT = "requestReport";
+		String RETRIEVE_REPORT = "retrieveReport";
+
+		@Output(REQ_REPORT)
+		MessageChannel requestReportChannel();
+
+		@Input(RETRIEVE_REPORT)
+		SubscribableChannel retrieveReportChannel();
+	}
+}
+
