f4c57563d7747b7eec0c250f31fdab2548e13e9e
veja001 <veli.khumalo@Velis-MacBook-Pro.local>
2017-01-12 15:23:16 +0200

    removed extra module

--- sky-reporting-service/src/main/java/za/co/absa/SkyReportingServiceApplication.java
+++ sky-reporting-service/src/main/java/za/co/absa/SkyReportingServiceApplication.java
@@ -10,3 +10,29 @@ public class SkyReportingServiceApplication {
         SpringApplication.run(SkyReportingServiceApplication.class, args);
     }
 }
+
+@EnableBinding(ReportingServiceProducer.Channels.class)
+class ReportingServiceProducer {
+
+    @InboundChannelAdapter(value = Channels.REQ_REPORT, poller = @Poller(fixedDelay = "10000"))
+    public String requestReportMessageSource(){
+        return "request:report";
+    }
+
+    @StreamListener(Channels.RETRIEVE_REPORT)
+    public void report(String message) {
+        System.out.println(">>>"+message);
+        new File("");
+    }
+
+    interface Channels {
+        String REQ_REPORT = "requestReport";
+        String RETRIEVE_REPORT = "retrieveReport";
+
+        @Output(REQ_REPORT)
+        MessageChannel requestReportChannel();
+
+        @Input(RETRIEVE_REPORT)
+        SubscribableChannel retrieveReportChannel();
+    }
+}
