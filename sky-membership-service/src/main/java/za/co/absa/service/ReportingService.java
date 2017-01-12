647a27ca8f9a69416c1c7f47d14f38af088079f9
bradloff <Wh@tSh0uld1Us3>
2017-01-12 22:45:46 +0200

    updated the file soe that the reporting service never returns an empty body, this causes issues for the receiving channel

--- sky-membership-service/src/main/java/za/co/absa/service/ReportingService.java
+++ sky-membership-service/src/main/java/za/co/absa/service/ReportingService.java
@@ -2,7 +2,11 @@ package za.co.absa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.integration.annotation.Transformer;
+import org.springframework.messaging.MessageChannel;
 import za.co.absa.model.Customer;
 
 import java.util.Collection;
@@ -10,19 +14,39 @@ import java.util.Collection;
 /**
  * Created by barry on 2017/01/12.
  */
-@EnableBinding
+@EnableBinding(ReportingService.Queues.class)
 public class ReportingService {
 
 
     @Autowired
     MembershipService memberService;
 
-    @Transformer(inputChannel = "reportRequest", outputChannel = "reportResponse")
-    public Collection<Customer> processReportingRequest(String timeData) {
+    @Transformer(inputChannel = Queues.INPUT, outputChannel = Queues.OUTPUT)
+    public String processReportingRequest(String timeData) {
         System.out.println("Reporting, getting all the customers");
-        return memberService.getAllCustomers();
+        StringBuffer data = new StringBuffer();
+
+        for(Customer customer: memberService.getAllCustomers()){
+            data.append("\n"); //hack to display nicely on the reporting service... ;)
+            data.append(customer.toString());
+        }
+        if(data.toString().length()==0){
+            data.append("No customer data available");
+        }
+        return data.toString();
 
 
+    }
+
+    interface Queues{
+        String INPUT  = "reportRequest";
+        String OUTPUT = "reportResponse";
+
+        @Input(INPUT)
+        MessageChannel reportRequest();
+
+        @Output(OUTPUT)
+        MessageChannel reportResponse();
     }
 
 }
